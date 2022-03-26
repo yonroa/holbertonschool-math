c49ec984687923f5e03667d542c306478a7c9325
yonroa <yonanderson11@gmail.com>
2022-03-26 16:12:20 -0500

    Fixing display

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,17 +7,26 @@
  */
 void display_complex_number(complex c)
 {
-	printf("%0.f", c.re);
-	if (c.im > 0)
-		printf(" + ");
-	else if (c.im < 0)
+	if (c.re == 0)
 	{
-		printf(" - ");
-		c.im = c.im * -1;
+		if (c.im < 0)
+		printf("- %.9gi\n", c.im * (-1));
+		else if (c.im == 0)
+		printf("0\n");
+		else
+		printf("%.9gi\n", c.im);
 	}
-	if (c.im == 1 || c.im == -1)
-		printf("i");
-	else if (c.im != 0)
-		printf("%0.fi", c.im);
-	printf("\n");
+	else
+	{
+		if (c.im < -1)
+		printf("%.9g - %.9gi\n", c.re, c.im * (-1));
+		else if (c.im == -1)
+			printf("%.9g - i\n", c.re);
+		else if (c.im == 0)
+			printf("%.9g\n", c.re);
+		else if (c.im == 1)
+			printf("%.9g + i\n", c.re);
+		else
+			printf("%.9g + %.9gi\n", c.re, c.im);
+	}
 }
